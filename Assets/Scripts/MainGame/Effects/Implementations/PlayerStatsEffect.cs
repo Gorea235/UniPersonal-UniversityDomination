1a180ac359415b03ea2050e9f5bc2fea2ec26915
Luke Davies <lkdvs3@gmail.com>
2018-05-10 14:01:50 +0100

    Fixed attack/defence stats being able to go below 1/0 for units/player

--- Assets/Scripts/MainGame/Effects/Implementations/PlayerStatsEffect.cs
+++ Assets/Scripts/MainGame/Effects/Implementations/PlayerStatsEffect.cs
@@ -3,91 +3,92 @@ using System.Linq;
 
 namespace EffectImpl
 {
-    [Serializable]
-    public class PlayerStatsEffect : Effect
-    {
-        #region Private Fields
+	[Serializable]
+	public class PlayerStatsEffect : Effect
+	{
+		#region Private Fields
 
-        string _name;
-        string _description;
-        int _attackModifier;
-        int _defenceModifier;
-        bool _friendly; // adhoc gallore. True: target friendly, False: target hostile;
+		string _name;
+		string _description;
+		int _attackModifier;
+		int _defenceModifier;
+		bool _friendly; // adhoc gallore. True: target friendly, False: target hostile;
 
-        #endregion
+		#endregion
 
-        #region Override Properties
+		#region Override Properties
 
-        public override string CardName => _name;
+		public override string CardName => _name;
 
-        public override string CardDescription => _description;
+		public override string CardDescription => _description;
 
-        public override CardCornerIcon CardCornerIcon => _friendly ? CardCornerIcon.SelfPlayer : CardCornerIcon.EnemyPlayer;
+		public override CardCornerIcon CardCornerIcon => _friendly ? CardCornerIcon.SelfPlayer : CardCornerIcon.EnemyPlayer;
 
-        public override CardTier CardTier => CardTier.Tier2;
+		public override CardTier CardTier => CardTier.Tier2;
 
-        public override int? AttackBonus => _attackModifier;
+		public override int? AttackBonus => _attackModifier;
 
-        public override int? DefenceBonus => _defenceModifier;
+		public override int? DefenceBonus => _defenceModifier;
 
-        #endregion
+		#endregion
 
-        #region Constructor
+		#region Constructor
 
-        public PlayerStatsEffect(CardType type)
-        {
-            switch (type)
-            {
-                case CardType.KudaWithTheLads:
-                    _name = "Kuda With The Lads";
-                    _description = "Increase the attack of your units by 1";
-                    _attackModifier = 1;
-                    _friendly = true;
-                    break;
-                case CardType.BreadcrumbFactory:
-                    _name = "Breadcrumb factory";
-                    _description = "Increase the defence of your units by 1";
-                    _defenceModifier = 1;
-                    _friendly = true;
-                    break;
-                case CardType.ArguingOverBars:
-                    _name = "Arguing Over Bars";
-                    _description = "Decrease target player's units attack by 1 ";
-                    _attackModifier = -1;
-                    _friendly = false;
-                    break;
-                case CardType.BadIntentionsSTYC:
-                    _name = "Bad Intentions STYC";
-                    _description = "Decrease target player's units defence by 1";
-                    _defenceModifier = -1;
-                    _friendly = false;
-                    break;
-                default:
-                    throw new InvalidOperationException();
-            }
-        }
+		public PlayerStatsEffect(CardType type)
+		{
+			switch (type)
+			{
+				case CardType.KudaWithTheLads:
+					_name = "Kuda With The Lads";
+					_description = "Increase the attack of your units by 1";
+					_attackModifier = 1;
+					_friendly = true;
+					break;
+				case CardType.BreadcrumbFactory:
+					_name = "Breadcrumb factory";
+					_description = "Increase the defence of your units by 1";
+					_defenceModifier = 1;
+					_friendly = true;
+					break;
+				case CardType.ArguingOverBars:
+					_name = "Arguing Over Bars";
+					_description = "Decrease target player's units attack by 1 ";
+					_attackModifier = -1;
+					_friendly = false;
+					break;
+				case CardType.BadIntentionsSTYC:
+					_name = "Bad Intentions STYC";
+					_description = "Decrease target player's units defence by 1";
+					_defenceModifier = -1;
+					_friendly = false;
+					break;
+				default:
+					throw new InvalidOperationException();
+			}
+		}
 
-        #endregion
+		#endregion
 
-        #region Concrete Methods
+		#region Concrete Methods
 
-        public override EffectAvailableSelection AvailableSelection(Game game) => new EffectAvailableSelection
-        {
-            Players = _friendly ?
-                      // get friendly units
-                      new[] { game.CurrentPlayer } :
-                      // get enemy units
-                      game.Players.Where(p => !p.IsEliminated && p != game.CurrentPlayer)
+		public override EffectAvailableSelection AvailableSelection(Game game) => new EffectAvailableSelection
+		{
+			Players = _friendly ?
+					  // current player
+					  new[] { game.CurrentPlayer } :
+					  // enemy players
+					  game.Players.Where(p => !p.IsEliminated && p != game.CurrentPlayer &&
+										 (_attackModifier < 0 ? p.Stats.Attack > 0 : p.Stats.Defence > 0))
 
-        };
+		};
 
-        #endregion
+		#endregion
 
-        #region Helper Methods
+		#region Helper Methods
 
-        protected override void ApplyToPlayer()
-        { }
+		protected override void ApplyToPlayer()
+		{ }
 
-        #endregion
-    }
+		#endregion
+	}
 }
--- Assets/Scripts/MainGame/Effects/Implementations/UnitStatsEffect.cs
+++ Assets/Scripts/MainGame/Effects/Implementations/UnitStatsEffect.cs
@@ -3,91 +3,92 @@ using System.Linq;
 
 namespace EffectImpl
 {
-    [Serializable]
-    public class UnitStatsEffect : Effect
-    {
-        #region Private Fields
+	[Serializable]
+	public class UnitStatsEffect : Effect
+	{
+		#region Private Fields
 
-        string _name;
-        string _description;
-        int _attackModifier;
-        int _defenceModifier;
-        bool _friendly; // adhoc gallore. True: target friendly, False: target hostile;
+		string _name;
+		string _description;
+		int _attackModifier;
+		int _defenceModifier;
+		bool _friendly; // adhoc gallore. True: target friendly, False: target hostile;
 
-        #endregion
+		#endregion
 
-        #region Override Properties
+		#region Override Properties
 
-        public override string CardName => _name;
+		public override string CardName => _name;
 
-        public override string CardDescription => _description;
+		public override string CardDescription => _description;
 
-        public override CardCornerIcon CardCornerIcon => _friendly ? CardCornerIcon.SelfUnit : CardCornerIcon.EnemyUnit;
+		public override CardCornerIcon CardCornerIcon => _friendly ? CardCornerIcon.SelfUnit : CardCornerIcon.EnemyUnit;
 
-        public override CardTier CardTier => CardTier.Tier1;
+		public override CardTier CardTier => CardTier.Tier1;
 
-        public override int? AttackBonus => _attackModifier;
+		public override int? AttackBonus => _attackModifier;
 
-        public override int? DefenceBonus => _defenceModifier;
+		public override int? DefenceBonus => _defenceModifier;
 
-        #endregion
+		#endregion
 
-        #region Constructor
+		#region Constructor
 
-        public UnitStatsEffect(CardType type)
-        {
-            switch (type)
-            {
-                case CardType.Kuda:
-                    _name = "Kuda";
-                    _description = "Increase the attack of a unit by 1";
-                    _attackModifier = 1;
-                    _friendly = true;
-                    break;
-                case CardType.Breadcrumbs:
-                    _name = "Breadcrumbs";
-                    _description = "Increase the defence of a unit by 1";
-                    _defenceModifier = 1;
-                    _friendly = true;
-                    break;
-                case CardType.FirstYearInTheLibrary:
-                    _name = "First Year In The Library";
-                    _description = "Decrease the attack of a unit by 1";
-                    _attackModifier = -1;
-                    _friendly = false;
-                    break;
-                case CardType.NightBeforeExams:
-                    _name = "Night Before Exams";
-                    _description = "Decrease the defence of a unit by 1";
-                    _defenceModifier = -1;
-                    _friendly = false;
-                    break;
-                default:
-                    throw new InvalidOperationException();
-            }
-        }
+		public UnitStatsEffect(CardType type)
+		{
+			switch (type)
+			{
+				case CardType.Kuda:
+					_name = "Kuda";
+					_description = "Increase the attack of a unit by 1";
+					_attackModifier = 1;
+					_friendly = true;
+					break;
+				case CardType.Breadcrumbs:
+					_name = "Breadcrumbs";
+					_description = "Increase the defence of a unit by 1";
+					_defenceModifier = 1;
+					_friendly = true;
+					break;
+				case CardType.FirstYearInTheLibrary:
+					_name = "First Year In The Library";
+					_description = "Decrease the attack of a unit by 1";
+					_attackModifier = -1;
+					_friendly = false;
+					break;
+				case CardType.NightBeforeExams:
+					_name = "Night Before Exams";
+					_description = "Decrease the defence of a unit by 1";
+					_defenceModifier = -1;
+					_friendly = false;
+					break;
+				default:
+					throw new InvalidOperationException();
+			}
+		}
 
-        #endregion
+		#endregion
 
-        #region Concrete Methods
+		#region Concrete Methods
 
-        public override EffectAvailableSelection AvailableSelection(Game game) => new EffectAvailableSelection
-        {
-            Units = _friendly ?
-                    // get friendly units
-                    game.CurrentPlayer.Units :
-                    // get enemy units
-                    game.Map.Sectors.Select(s => s.Unit).Where(u => u != null && u.Owner != game.CurrentPlayer)
+		public override EffectAvailableSelection AvailableSelection(Game game) => new EffectAvailableSelection
+		{
+			Units = _friendly ?
+					// get friendly units
+					game.CurrentPlayer.Units :
+					// get enemy units
+					game.Map.Sectors.Select(s => s.Unit).Where(u => u != null && u.Owner != game.CurrentPlayer &&
+															   (_attackModifier < 0 ? u.TotalAttack > 1 : u.TotalDefence > 1))
 
-        };
+		};
 
-        #endregion
+		#endregion
 
-        #region Helper Methods
+		#region Helper Methods
 
-        protected override void ApplyToUnit()
-        { }
+		protected override void ApplyToUnit()
+		{ }
 
-        #endregion
-    }
+		#endregion
+	}
 }
