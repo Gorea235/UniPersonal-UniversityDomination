bcc163ad85d996b8843f9e49334d6f1de7da30db
Luke Davies <lkdvs3@gmail.com>
2018-05-10 14:29:02 +0100

    Added webGL build support

--- Assets/Editor/BuildHelper.cs
+++ Assets/Editor/BuildHelper.cs
@@ -5,27 +5,28 @@ using UnityEditor;
 
 public static class BuildHelper
 {
-    static Lazy<BuildOptions> _currentOptions = new Lazy<BuildOptions>(
-        () => System.Environment.GetCommandLineArgs().Contains("-arg-debug") ?
-        BuildOptions.Development | BuildOptions.AllowDebugging :
-        BuildOptions.None);
+	static Lazy<BuildOptions> _currentOptions = new Lazy<BuildOptions>(
+		() => System.Environment.GetCommandLineArgs().Contains("-arg-debug") ?
+		BuildOptions.Development | BuildOptions.AllowDebugging :
+		BuildOptions.None);
 
-    public static BuildOptions CurrentBuildOptions => _currentOptions.Value;
+	public static BuildOptions CurrentBuildOptions => _currentOptions.Value;
 
-    public static IEnumerable<Tuple<string, BuildTarget>> BuildNames(bool includeiOS)
-    {
+	public static IEnumerable<Tuple<string, BuildTarget>> BuildNames(bool includeiOS)
+	{
 #if UNITY_2017_3_OR_NEWER
-        yield return Tuple.Create("macOS", BuildTarget.StandaloneOSX);
+		yield return Tuple.Create("macOS", BuildTarget.StandaloneOSX);
 #else
         yield return Tuple.Create("macOS", BuildTarget.StandaloneOSXIntel64);
 #endif
-        yield return Tuple.Create("win32", BuildTarget.StandaloneWindows);
-        yield return Tuple.Create("win64", BuildTarget.StandaloneWindows64);
-        yield return Tuple.Create("android", BuildTarget.Android);
-        if (includeiOS)
-            yield return Tuple.Create("ios", BuildTarget.iOS);
-        yield return Tuple.Create("linux-universal", BuildTarget.StandaloneLinuxUniversal);
-        yield return Tuple.Create("linux32", BuildTarget.StandaloneLinux);
-        yield return Tuple.Create("linux64", BuildTarget.StandaloneLinux64);
-    }
+		yield return Tuple.Create("win32", BuildTarget.StandaloneWindows);
+		yield return Tuple.Create("win64", BuildTarget.StandaloneWindows64);
+		yield return Tuple.Create("android", BuildTarget.Android);
+		if (includeiOS)
+			yield return Tuple.Create("ios", BuildTarget.iOS);
+		yield return Tuple.Create("linux-universal", BuildTarget.StandaloneLinuxUniversal);
+		yield return Tuple.Create("linux32", BuildTarget.StandaloneLinux);
+		yield return Tuple.Create("linux64", BuildTarget.StandaloneLinux64);
+		yield return Tuple.Create("webGL", BuildTarget.WebGL);
+	}
 }
